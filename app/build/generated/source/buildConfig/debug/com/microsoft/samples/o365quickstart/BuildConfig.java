c3c70f9049aad5ec4ee84ef29413f867a5504396
gmaster <grandmasteraziz@hotmail.com>
2019-07-30 19:00:28 +0300

    second commit

---
+++ app/build/generated/source/buildConfig/debug/com/microsoft/samples/o365quickstart/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.microsoft.samples.o365quickstart;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.microsoft.samples.outlookquickstart";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
